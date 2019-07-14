de000f0841883aa28fe082a6d9da5650b9244fea
keriali123 <1241454312@qq.com>
2019-07-14 23:20:32 +0800

    post_detail

--- InstaDemo/user/urls.py
+++ InstaDemo/user/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path("",views.HelloDjango.as_view())
+    # path("",views.HelloDjango.as_view())
+    path("home/",views.PostListView.as_view(),name="home"),
+    path("post/<int:pk>/",views.PostDetailView.as_view(),name="post_detail")
+    
 
 
 
--- InstaDemo/user/views.py
+++ InstaDemo/user/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView,DetailView
+from .models import *
 
 # Create your views here.
 
 
 class HelloDjango(TemplateView):
     template_name="home.html"
+
+
+class PostListView(ListView):
+    model=Post
+    template_name="post.html"
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name="post_detail.html"
